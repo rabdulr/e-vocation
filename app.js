bdd6e84fea1652d4c49035afceba7c3846fa139b
Neel <krishlal925@gmail.com>
2020-04-04 12:39:51 -0700

    adding auth routes to app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const db = require("./db");
+const jwt = require("jwt-simple");
+const models = db.models;
 
 app.use('/dist', express.static(path.join(__dirname, 'dist')));
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
@@ -10,5 +13,41 @@ app.get('/', (req, res, next) => {
 });
 
 app.use(express.json());
+
+const isLoggedIn = (req, res, next) => {
+    if (!req.user) {
+      const error = Error("not authorized");
+      error.status = 401;
+      return next(error);
+    }
+    next();
+  };
+
+app.post("/api/auth", (req, res, next) => {
+    db.authenticate(req.body)
+      .then((token) => res.send({ token }))
+      .catch(() => {
+        const error = Error("not authorized");
+        error.status = 401;
+        next(error);
+      });
+  });
+
+  app.get("/api/auth", isLoggedIn, (req, res, next) => {
+    res.send(req.user);
+  });
+
+// error messaging
+app.use((req, res, next) => {
+    const error = {
+        message: `page not found ${req.url} for ${req.method}`,
+        status: 404,
+    };
+    next(error);
+});
+app.use((err, req, res, next) => {
+    console.log(err.status, err.message);
+    res.status(err.status || 500).send({ message: err.message });
+});
 
 module.exports = app
