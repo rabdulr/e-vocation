8617a1666876615b72b53129e644ea1761d5a89a
リンゴいちご <codyfrazier@Wanderers-MacBook-Air.local>
2020-04-09 15:06:21 -0700

    Created Sign Up Form

--- src/SignInForm.js
+++ src/SignInForm.js
@@ -1,38 +1,35 @@
 import React, { useState, useEffect } from 'react';
 
-const LoginForm = ({ displayLogin, login, toggleForm }) => {
+const SignInForm = ({ displayLogin, login, toggleForm }) => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
-    const onSubmit = (ev) => {
-        ev.preventDefault();
-        displayLogin();
-        login({ username, password })
-            .catch(ex => console.log(ex))
+    const onSubmit = ({ target }) => {
+        event.preventDefault();
+        console.log("I'm a submission!")
     }
-    
     return (
         <div className = 'columnNW alignCenter z1 widthundred heighthundred bgAlphaDB frosted'>
             <div className = 'rowNW justifyEnd widthundred'>
                 <input type = 'button' onClick = { () => displayLogin() } className = 'border50 colorAO bgTransparent topMargin2 rightMarginHalf borderAO padQuarter sixteenPoint' value = ' X '/>
             </div>
-            <h2 className = 'colorLB'>Login</h2>
+            <h2 className = 'colorLB'>Sign Up</h2>
             <form className = 'columnNW borderAO bgBB colorOW pad1 margin1 border5 maxWidth3' onSubmit={ onSubmit }>
                 <div className = 'rowNW spaceBetweenRow widthundred'>
                     <div>Username: </div>
-                    <input className = 'bgLB colorDB borderDB padQuarter' placeholder = 'Type Username...' pattern = '.{3,}' value = { username } onChange = { ({ target }) => setUsername(target.value) }  />
+                    <input className = 'bgLB colorDB borderDB padQuarter' placeholder = 'New Username...' pattern = '.{3,}' value = { username } onChange = { ({ target }) => setUsername(target.value) }  />
                 </div>
                 <div className = 'rowNW spaceBetweenRow topMargin1 widthundred'>
-                    <div>Password: </div><input className = 'bgLB colorDB borderDB padQuarter' type = 'password' placeholder = 'Type Password...' pattern = '.{8,}' value = { password } onChange = { ({ target }) => { setPassword(target.value) }} />
+                    <div>Password: </div>
+                    <input className = 'bgLB colorDB borderDB padQuarter' type = 'password' placeholder = 'New Password...' pattern = '.{8,}' value = { password } onChange = { ({ target }) => { setPassword(target.value) }} />
                 </div>
-                <input type = 'submit' className = 'bgDB colorAO borderAO topMargin1 padQuarter' value = 'Log In' />
+                <input type = 'submit' className = 'bgDB colorAO borderAO topMargin1 padQuarter' value = 'Sign Up' />
                 <div className = 'rowNW justifyEnd widthundred'>
-                    <div onClick = { () => toggleForm() } className = 'colorAO bgTransparent topMarginHalf'>Sign Up</div>
+                    <div onClick = { () => toggleForm() } className = 'colorAO bgTransparent topMarginHalf'>Login</div>
                 </div>
             </form>
-            
         </div>
     );
 };
 
-export default LoginForm;
+export default SignInForm;
