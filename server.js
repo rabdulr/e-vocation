1c721ac2efc63037b8010893f52e616f7c87355d
Neel <krishlal925@gmail.com>
2020-04-09 20:08:41 -0700

    first commit for web-sockets

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const db = require('./db');
 const app = require('./app');
+const io = require('socket.io')
 
 const PORT = process.env.PORT || 3000;
 
 db.sync()
     .then(() => {
-        app.listen(PORT, () => {
+        const server = app.listen(PORT, () => {
             console.log(`Listening on port ${PORT}`)
         });
-    });  
+
+        const socketServer = io(server);
+    });
