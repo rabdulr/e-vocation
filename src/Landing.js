82a7252246f5d7942678803f11f0e0a0f2c03fa8
リンゴいちご <codyfrazier@Wanderers-MacBook-Air.local>
2020-04-10 01:11:12 -0700

    some minor fixes

--- src/Landing.js
+++ src/Landing.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
 
-const Landing = (({ displayLogin }) => {
+const Landing = ({ displayLogin, route }) => {
   return(<div id='LandingRoot' className='rowNW'>
     <div className='marginHalf justifyContent'>
       <h2>Find a Job Near You. Gluten-Free!</h2>
       <p>so many jobs!</p>
-      <input type='text' value='search jobs'></input>
+      <input type='text' placeholder='search jobs'></input>
     </div>
     <div className='marginHalf'>
       <h2>Find Local Labor without Having Kids!</h2>
@@ -13,6 +13,6 @@ const Landing = (({ displayLogin }) => {
       <button onClick = {() => displayLogin()}>Login</button>
     </div>
   </div>)
-})
+}
 
-export default Landing
+export default Landing;
