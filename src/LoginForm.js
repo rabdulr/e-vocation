2e6fc2d1c652fb01232e3497bfe8a3f5b9229209
リンゴいちご <codyfrazier@Wanderers-MacBook-Air.local>
2020-04-10 13:02:04 -0700

    added required to all login/signup forms

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -20,10 +20,10 @@ const LoginForm = ({ displayLogin, login, toggleForm }) => {
             <form className = 'columnNW borderAO bgBB colorOW pad1 margin1 border5 maxWidth3' onSubmit={ onSubmit }>
                 <div className = 'rowNW spaceBetweenRow widthundred'>
                     <div>Username: </div>
-                    <input className = 'bgLB colorDB borderDB padQuarter' placeholder = 'Type Username...' pattern = '.{3,}' value = { username } onChange = { ({ target }) => setUsername(target.value) }  />
+                    <input className = 'bgLB colorDB borderDB padQuarter' placeholder = 'Type Username...' pattern = '.{3,}' value = { username } onChange = { ({ target }) => setUsername(target.value) } required/>
                 </div>
                 <div className = 'rowNW spaceBetweenRow topMargin1 widthundred'>
-                    <div>Password: </div><input className = 'bgLB colorDB borderDB padQuarter' type = 'password' placeholder = 'Type Password...' pattern = '.{8,}' value = { password } onChange = { ({ target }) => { setPassword(target.value) }} />
+                    <div>Password: </div><input className = 'bgLB colorDB borderDB padQuarter' type = 'password' placeholder = 'Type Password...' pattern = '.{8,}' value = { password } onChange = { ({ target }) => { setPassword(target.value) }} required/>
                 </div>
                 <input type = 'submit' className = 'bgDB colorAO borderAO topMargin1 padQuarter' value = 'Log In' />
                 <div className = 'rowNW justifyEnd widthundred'>
--- src/SignInForm.js
+++ src/SignInForm.js
@@ -35,47 +35,47 @@ const SignInForm = ({ displayLogin, login, toggleForm }) => {
             <form className = 'columnNW borderAO bgBB colorOW pad1 margin1 border5 maxWidth3 scrollable maxHeight3' onSubmit={ onSubmit }>
                 <div className = 'rowNW spaceBetweenRow widthundred'>
                     <div>Company Account: </div>
-                    <input type = 'checkbox' onClick = { () => setisCompany(!isCompany) }/>
+                    <input type = 'checkbox' onClick = { () => setisCompany(!isCompany) } />
                 </div>
                 <div className = 'rowNW spaceBetweenRow topMargin1 widthundred'>
                     <div>First Name: </div>
-                    <input className = 'bgLB colorDB borderDB padQuarter' placeholder = 'First Name...' pattern = '.{1,}' value = { firstname } onChange = { ({ target }) => setFirstname(target.value) } />
+                    <input className = 'bgLB colorDB borderDB padQuarter' placeholder = 'First Name...' pattern = '.{1,}' value = { firstname } onChange = { ({ target }) => setFirstname(target.value) } required/>
                 </div>
                 <div className = 'rowNW spaceBetweenRow topMargin1 widthundred'>
                     <div>lastName: </div>
-                    <input className = 'bgLB colorDB borderDB padQuarter' placeholder = 'Last Name...' pattern = '.{1,}' value = { lastName } onChange = { ({ target }) => setlastName(target.value) } />
+                    <input className = 'bgLB colorDB borderDB padQuarter' placeholder = 'Last Name...' pattern = '.{1,}' value = { lastName } onChange = { ({ target }) => setlastName(target.value) } required/>
                 </div>
                 <div className = 'rowNW spaceBetweenRow topMargin1 widthundred'>
                     <div>Address: </div>
-                    <input className = 'bgLB colorDB borderDB padQuarter' placeholder = 'Primary Address...' pattern = '.{1,}' value = { address } onChange = { ({ target }) => setAddress(target.value) } />
+                    <input className = 'bgLB colorDB borderDB padQuarter' placeholder = 'Primary Address...' pattern = '.{1,}' value = { address } onChange = { ({ target }) => setAddress(target.value) } required/>
                 </div>
                 <div className = 'rowNW spaceBetweenRow topMargin1 widthundred'>
                     <div>City: </div>
-                    <input className = 'bgLB colorDB borderDB padQuarter' placeholder = 'City...' pattern = '.{1,}' value = { city } onChange = { ({ target }) => setCity(target.value) } />
+                    <input className = 'bgLB colorDB borderDB padQuarter' placeholder = 'City...' pattern = '.{1,}' value = { city } onChange = { ({ target }) => setCity(target.value) } required/>
                 </div>
                 <div className = 'rowNW spaceBetweenRow topMargin1 widthundred'>
                     <div>State: </div>
-                    <input className = 'bgLB colorDB borderDB padQuarter' placeholder = 'State Abreviation...' pattern = '.{2,2}' value = { state } onChange = { ({ target }) => setState(target.value) } />
+                    <input className = 'bgLB colorDB borderDB padQuarter' placeholder = 'State Abreviation...' pattern = '.{2,2}' value = { state } onChange = { ({ target }) => setState(target.value) } required/>
                 </div>
                 <div className = 'rowNW spaceBetweenRow topMargin1 widthundred'>
                     <div>Zip Code: </div>
-                    <input className = 'bgLB colorDB borderDB padQuarter' placeholder = 'Zip Code...' pattern = '.{5,6}' value = { zip } onChange = { ({ target }) => setZip(target.value) } />
+                    <input className = 'bgLB colorDB borderDB padQuarter' placeholder = 'Zip Code...' pattern = '.{5,6}' value = { zip } onChange = { ({ target }) => setZip(target.value) } required/>
                 </div>
                 { isCompany && <div className = 'rowNW spaceBetweenRow topMargin1 widthundred'>
                     <div>Company: </div>
-                    <input className = 'bgLB colorDB borderDB padQuarter' placeholder = 'New Company...' pattern = '.{1,}' value = { companyName } onChange = { ({ target }) => setcompanyName(target.value) } />
+                    <input className = 'bgLB colorDB borderDB padQuarter' placeholder = 'New Company...' pattern = '.{1,}' value = { companyName } onChange = { ({ target }) => setcompanyName(target.value) } required = { isCompany ? true : false }/>
                 </div> }
                 { isCompany && <div className = 'rowNW spaceBetweenRow topMargin1 widthundred'>
                     <div>Industry: </div>
-                    <input className = 'bgLB colorDB borderDB padQuarter' placeholder = 'Primary Industry...' pattern = '.{1,}' value = { industry } onChange = { ({ target }) => setIndustry(target.value) } />
+                    <input className = 'bgLB colorDB borderDB padQuarter' placeholder = 'Primary Industry...' pattern = '.{1,}' value = { industry } onChange = { ({ target }) => setIndustry(target.value) } required = { isCompany ? true: false }/>
                 </div> }
                 <div className = 'rowNW spaceBetweenRow topMargin1 widthundred'>
                     <div>Username: </div>
-                    <input className = 'bgLB colorDB borderDB padQuarter' placeholder = 'New Username...' pattern = '.{3,}' value = { username } onChange = { ({ target }) => setUsername(target.value) }  />
+                    <input className = 'bgLB colorDB borderDB padQuarter' placeholder = 'New Username...' pattern = '.{3,}' value = { username } onChange = { ({ target }) => setUsername(target.value) } required/>
                 </div>
                 <div className = 'rowNW spaceBetweenRow topMargin1 widthundred'>
                     <div>Password: </div>
-                    <input className = 'bgLB colorDB borderDB padQuarter' type = 'password' placeholder = 'New Password...' pattern = '.{8,}' value = { password } onChange = { ({ target }) => { setPassword(target.value) }} />
+                    <input className = 'bgLB colorDB borderDB padQuarter' type = 'password' placeholder = 'New Password...' pattern = '.{8,}' value = { password } onChange = { ({ target }) => { setPassword(target.value) }} required/>
                 </div>
                 <input type = 'submit' className = 'bgDB colorAO borderAO topMargin1 padQuarter' value = 'Sign Up' />
                 <div className = 'rowNW justifyEnd widthundred'>
