33156a9c3a800937b4d98523735d29563476c30d
リンゴいちご <codyfrazier@Wanderers-MacBook-Air.local>
2020-04-12 02:18:47 -0700

    Build Out Chat Display

--- src/chatpage.js
+++ src/chatpage.js
@@ -8,18 +8,21 @@ const ChatPage = ({chatMessages, setChatMessages, displayChat, socket, auth})=>{
         ev.preventDefault();
         const socket = io();
         socket.emit('message', {username: auth.username, text: message})
+        setMessage('');
     }
     return(
-        <div>
-            <form onSubmit={onSubmit}>
-                <h3>Chat with { auth.id }</h3>
-                <input onChange= {(ev)=>setMessage(ev.target.value) } value = {message}></input>
-                <button>Send</button>
-            </form>
-            <ul id = 'messages'>
-
-            </ul>
-        </div>
+        <div className = 'columnNW maxWidth5'>
+            <h3 className = 'centerText'>Chat with { auth.id }</h3>
+            <div className = 'borderDB bgBB border5 marginHalf heighthundred'>
+                <ul id = 'messages' className = 'borderDB marginHalf bgOW colorDB border10 scrollable padHalf maxHeight3'></ul>
+                <form onSubmit={onSubmit} className = 'columnNW leftMarginHalf rightMarginHalf'>
+                    <div className = 'rowNW widthundred'>
+                        <input onChange= {({ target })=>setMessage(target.value) } value = {message} className = 'bottomLeft10 topLeft10 bgOW colorDB borderDB leftPadHalf widthundred' />
+                        <input type = 'submit' className = 'bgOW colorDB borderDB bottomRight10 topRight10' value = 'Send' />    
+                    </div>
+                </form>
+            </div>    
+        </div>   
     )
 }
 
