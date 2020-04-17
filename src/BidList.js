328ec1ca7791354b2fa8e54a6c2b4a2d370e2321
XtremePineapple <95henryX@gmail.com>
2020-04-17 13:32:29 -0700

    expanded BidList

--- src/BidList.js
+++ src/BidList.js
@@ -2,14 +2,36 @@ import React from 'react';
 
 const BidList = ({bids, users}) => {
 
-  console.log(bids)
-  console.log(users)
+  const Accept = () => {
+    alert("//TODO move this bid to the top, call the backEnd, disable other bids, initiate contract?, email bidder?")
+  }
+  const Message = () => {
+    alert("//TODO route to ChatPage between users")
+  }
+  const Delete = () => {
+    alert("//TODO remove bid, set to 'deleted' on backEnd (to prevent a re-bid?)")
+  }
 
-  return(<ul>
+  return(<ul className = "rowWrap">
     { bids.map(bid=>{
-      return(<li key={bid.bidderId} className = "borderBB">
-        <h3>Bid from {users.filter(use => use.id === bid.bidderId)[0].companyName}</h3>
+      let statusColor = ''
+      if(bid.bidStatus === 'Active'){
+        statusColor = 'colorG'
+      // } else if(bid.bidStatus === 'Accepted') {
+      //   statusColor = 'colorDB'
+      }
+      
+      return(<li key={bid.bidderId} className = "borderBB widthundred marginHalf padHalf">
+        <div className = "rowNW spaceBetweenRow">
+          <h3>Bid from {users.filter(use => use.id === bid.bidderId)[0].companyName}</h3>
+          <h4 className = {"topMarginAuto " +statusColor} >{bid.bidStatus}</h4>
+        </div> 
         <p>{bid.proposal}</p>
+        <div className = "rowNW spaceBetweenRow">
+          <button onClick = { ()=>Accept() }>Accept</button>
+          <button onClick = { ()=>Message() }>Message</button>
+          <button onClick = { ()=>Delete() }>Delete</button>
+        </div>
       </li>)
     }) }
   </ul>)
