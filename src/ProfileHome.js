5405a6786570a57dc76d5ddeeb58ee849053d2ba
Neel <krishlal925@gmail.com>
2020-04-25 11:47:46 -0700

    moved buttons, chat conversation links now working

--- src/ProfileHome.js
+++ src/ProfileHome.js
@@ -15,7 +15,7 @@ const ProfileHome = ({ auth, bids, posts, setPosts, breakpoint, route, setFocus 
         route('#');
     }
   }, []);
-  
+
   useEffect(() => {
     auth.role === 'COMPANY' ? setList([...bids]) : setList([...posts]);
   }, []);
@@ -24,28 +24,28 @@ const ProfileHome = ({ auth, bids, posts, setPosts, breakpoint, route, setFocus 
     <div className = { `${ breakpoint === 'sm' || breakpoint === 'md' ? 'columnNW alignCenter' : 'rowWrap spaceBetweenRow' } margin1` }>
       <div className = 'columnNW'>
         <h1>{ auth.firstName } { auth.lastName }</h1>
-        <ProfileCalendar  auth={auth} posts={posts} setFocus = {setFocus}/>
         <div className = 'rowWrap spaceBetweenRow'>
           <input type = 'button' className = 'bgDB colorAO borderLB border5 padQuarter' onClick={ ()=> { setFocus(auth.id); route(`#profile/settings/${ auth.id }`) } } value = 'Edit Profile' />
           <input type = 'button' className = 'bgDB colorAO borderLB border5 padQuarter' onClick={ ()=> route(`#job-history/${ auth.id }`) } value = 'History' />
-        </div>  
+        </div>
+        <ProfileCalendar  auth={auth} posts={posts} setFocus = {setFocus}/>
       </div>
       { list.length > 0 && <div className = 'columnNW'>
         <h3 className = 'topMargin1'>Most Recent { auth.role === 'COMPANY' ? 'Bids' : 'Jobs' }</h3>
-        <ul className = 'scrollable maxHeight2 bgLB border5 maxWidth4'>{ 
+        <ul className = 'scrollable maxHeight2 bgLB border5 maxWidth4'>{
           list.filter((item, idx) => idx < 5).map(item => {
             return (
               <li key = { `bid${ Math.ceil(Math.random() * 1000) }${ auth.role === 'COMPANY' ? item.bidderId : item.userId }` } className = 'bgAlphaDB colorLB border10 pad1 marginHalf'>{ auth.role === 'COMPANY' ? item.proposal : item.description }</li>
             )
           })
-        }</ul>  
+        }</ul>
       </div> }
       { list.length > 0 && <div className = 'columnNW'>
         <h3>Conversations</h3>
         <ul className = 'scrollable maxHeight2 maxWidth4'>{
           list.reduce((acc, item) => { if(!acc.includes(item)){ acc.push(item) }; return acc }, []).map(item => {
             return (
-              <li key = { `chat${ Math.ceil(Math.random() * 1000) }${ auth.role === 'COMPANY' ? item.bidderId : item.userId }` } className = 'bgDB marginHalf padHalf border5'><a href = { `#chat${ auth.role === 'COMPANY' ? item.bidderId : item.userId }` } className = 'colorBB' onClick = { ev => setFocus(auth.role === 'COMPANY' ? item.bidderId : item.userId) }>{ auth.role === 'COMPANY' ? item.bidderId : item.userId }</a></li>
+              <li key = { `chat${ Math.ceil(Math.random() * 1000) }${ auth.role === 'COMPANY' ? item.bidderId : item.userId }` } className = 'bgDB marginHalf padHalf border5'><a href = { `#view=chat&id=${ auth.role === 'COMPANY' ? item.bidderId : item.userId }` } className = 'colorBB' onClick = { ev => setFocus(auth.role === 'COMPANY' ? item.bidderId : item.userId) }>{ auth.role === 'COMPANY' ? item.bidderId : item.userId }</a></li>
             )
           })
         }</ul>
