1c61a8447591f4acbe5c6751add2b2b1305ee0f1
リンゴいちご <codyfrazier@Wanderers-MacBook-Air.local>
2020-04-29 00:06:53 -0700

    replaced auth.role with mode

--- src/Landing.js
+++ src/Landing.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Fuse from 'fuse.js';
 import moment from 'moment';
 
-const Landing = ({ displayLogin, auth, route, breakpoint, posts, searchReturn, setSearchReturn, result, searchList, setSearchList, searchTerms, setSearchTerms, searchContent, setSearchContent, submitSearch, updateTerms, setLandSearch }) => {
+const Landing = ({ displayLogin, auth, mode, route, breakpoint, posts, searchReturn, setSearchReturn, result, searchList, setSearchList, searchTerms, setSearchTerms, searchContent, setSearchContent, submitSearch, updateTerms, setLandSearch }) => {
 
   return(
     <div id='LandingRoot' className={ `${ breakpoint === 'sm' || breakpoint === 'md' ? 'columnNW' : 'rowNW' }` }>
@@ -10,12 +10,10 @@ const Landing = ({ displayLogin, auth, route, breakpoint, posts, searchReturn, s
         <h2>Find a Job Near You. Gluten-Free!</h2>
         <div id='map'></div>
         <p>so many jobs!</p>
-        { 
-          (auth.role === 'COMPANY' || auth.role === 'ADMIN') && <form onSubmit = { () => submitSearch(true) } className = 'rowNW widthundred marginRightHalf'>
+        { mode === 'COMPANY' && <form onSubmit = { () => submitSearch(true) } className = 'rowNW widthundred marginRightHalf'>
           <input placeholder='search jobs' value = { searchTerms.join(' ') } onChange = { ({ target }) => updateTerms(target.value) } className = 'bgLB colorDB topLeft15 bottomLeft15 borderDB padHalf widthundred' />  
           <input type = 'submit' value = 'Search' className = 'bgDB colorOW borderDB topRight15 bottomRight15 padHalf' />
-        </form> 
-        }
+        </form> }
       </div>
       <div className='marginHalf'>
         <h2>Find Local Labor without Having Kids!</h2>
