694b774078e163062ae5322ecdecca3347bd1c55
リンゴいちご <codyfrazier@Wanderers-MacBook-Air.local>
2020-04-30 21:54:24 -0700

    Final Styling for Posts

--- src/PostSearch.js
+++ src/PostSearch.js
@@ -25,15 +25,15 @@ const PostSearch = ({auth, posts, route, breakpoint, createJobPost}) => {
   }
 
   return(
-    <div id="PostSearchRoot" className = { `${ breakpoint === 'sm' || breakpoint === 'md' || breakpoint === 'lg' ? 'columnNW' : 'rowWrap' }` }>
+    <div id="PostSearchRoot" className = 'columnNW alignCenter'>
       { postDisplay && < PostForm auth = { auth } createJobPost = { createJobPost } userId = { userId } setUserId = { setUserId } togglePost = { togglePost } /> }
-      <div className = 'marginHalf columnNW alignCenter maxWidth4'>
+      <div className = { `marginHalf columnNW alignCenter ${ breakpoint === 'sm' ? 'maxWidth4' : '' }` }>
         <input type = 'button' value = 'Post New Job' onClick = { togglePost } className = 'EighteenPoint borderDB colorOW bgBB border15 padQuarter widthundred bottomMargin1' />
         <h2 className = 'colorDB '>Posts</h2>
-        <ul className = 'widthundred scrollable maxHeight4'>{ 
+        <ul className = { `widthundred scrollable ${ breakpoint === 'sm' ? 'maxHeight4 columnNW' : 'maxHeight7 rowWrap justifyStart' }` }>{ 
           posts.map(post => {
             return (
-            <li key = { `post${ post.id }` } className = 'colorDB'>
+            <li key = { `post${ post.id }` } className = { `colorDB ${ breakpoint === 'sm' ? '' : 'vw3' }` }>
               <a  href = {`#post/${post.id}`} >
                 <h4 className = 'leftMarginHalf colorDB'>{ post.title }</h4>
               </a>
