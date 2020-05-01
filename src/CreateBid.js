477ffd404cf92386eb2e99ca6e2a3fd2dd5eb57b
XtremePineapple <95henryX@gmail.com>
2020-04-30 21:49:46 -0700

    creating is possible

--- src/CreateBid.js
+++ src/CreateBid.js
@@ -2,11 +2,12 @@ import React, {useState, useEffect} from 'react';
 import { Bids } from '../db/models/constructors';
 
 
-const CreateBid = ({post, auth, createBid, bids}) => {
+const CreateBid = ({post, auth, createBid, singleBid}) => {
+    console.log('single', singleBid)
+    // console.log('auth', auth.id)
     const [proposal, setProposal] = useState('');
     const [bid, setBid] = useState('');
-    const [bidState, setBidState] = useState('false');
-    const [currentBid, setCurrentBid] = useState(bids.find(bid => bid.postId === post.id && bid.bidderId === auth.id))
+    const [currentBid, setCurrentBid] = useState({bidStatus: 'false'})
 
     const onSubmit =  (ev) => {
       ev.preventDefault();
@@ -15,49 +16,54 @@ const CreateBid = ({post, auth, createBid, bids}) => {
       setProposal('');
       setBid('');
     };
-
-    useEffect(() => {
-      if(currentBid){
-        setBidState('true');
+    
+    useEffect(()=>{
+      if(singleBid){
+        setCurrentBid(singleBid)
       }
-    }, [currentBid])
+    },[singleBid])
 
+    // useEffect(() => {
+    //   if(currentBid){
+    //     setBidState('true');
+    //   }
+    // }, [currentBid])
+
+  if(currentBid.bidStatus === "Active"){
     return(
       <div>
-          {
-            bidState === 'false' && 
-              <form onSubmit={onSubmit} className = 'columnNW bgLB border5 pad1'>
-                <div className = 'rowNW spaceBetweenRow'>
-                  <div className = 'colorDB'>Proposal: </div>
-                  <textarea placeholder='Proposal' value={ proposal } onChange={ev => setProposal(ev.target.value)} className = 'colorDB bgOW border5 borderDB leftPadHalf'/>
-                </div>
-                <div className = 'rowNW spaceBetweenRow topMarginHalf'>
-                  <div className = 'colorDB'>Bid: </div>
-                  <input type='text' placeholder='proposed bid' value={ bid } onChange={ev => setBid(ev.target.value)} className = 'colorDB bgOW border5 borderDB leftPadHalf'/>
-                </div>
-                <input type = 'button' className = 'bgBB colorDB borderDB topMarginHalf' value = 'Create New Bid' />
-              </form>  
-          }
-          {
-            bidState === 'true' && 
-            <div>
-              <h3>Status: {currentBid.bidStatus}</h3>
-              <p>
-                Submitted Proposal: {currentBid.proposal}
-              </p>
-              <p>
-                Bid: {currentBid.bid}
-              </p>
-            </div>
-          }
-          {
-            bidState === 'deleted' && 
-            <div>
-              <h3>Your bid has been closed.(</h3>
-            </div>
-          }
+        <h3>Status: {currentBid.bidStatus}</h3>
+        <p>
+          Submitted Proposal: {currentBid.proposal}
+        </p>
+        <p>
+          Bid: {currentBid.bid}
+        </p>
       </div>
     )
+  } else if(currentBid.bidStatus === "Deleted"){
+    return(
+      <div>
+        <h3>Your bid has been closed.</h3>
+      </div>
+    )
+  } else {
+  return(
+    <div>
+      <form onSubmit={onSubmit} className = 'columnNW bgLB border5 pad1'>
+        <div className = 'rowNW spaceBetweenRow'>
+          <div className = 'colorDB'>Proposal: </div>
+          <textarea placeholder='Proposal' value={ proposal } onChange={ev => setProposal(ev.target.value)} className = 'colorDB bgOW border5 borderDB leftPadHalf'/>
+        </div>
+        <div className = 'rowNW spaceBetweenRow topMarginHalf'>
+          <div className = 'colorDB'>Bid: </div>
+          <input type='text' placeholder='proposed bid' value={ bid } onChange={ev => setBid(ev.target.value)} className = 'colorDB bgOW border5 borderDB leftPadHalf'/>
+        </div>
+        <input type = 'button' className = 'bgBB colorDB borderDB topMarginHalf' value = 'Create New Bid' />
+      </form>
+    </div>
+  )}
+
 }
 
 export default CreateBid;
