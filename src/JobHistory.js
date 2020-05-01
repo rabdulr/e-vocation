d5db8ecedef7476287ae22f08727e5cc3d6a964d
Redzuan <rabdulr@icloud.com>
2020-04-30 22:17:55 -0700

    changes to job history

--- src/JobHistory.js
+++ src/JobHistory.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
 const JobHistory = ({ auth, route, posts, breakpoint }) => {
+    const [history, setHistory] = useState([])
 
     useEffect(() => {
         if(!(auth.id)){
@@ -8,22 +9,28 @@ const JobHistory = ({ auth, route, posts, breakpoint }) => {
         }
     }, [])
 
+    useEffect(() => {
+        if(posts) {
+            setHistory(posts)
+        }
+    }, [posts])
+
     return (
         <div className = { `${ breakpoint === 'sm' || breakpoint === 'md' || breakpoint === 'lg' ? 'columnNW' : 'rowNW' }` }>
             <h3>{ auth.username } Job History</h3>
             <h4>Posted Jobs</h4>
             <ul>{
-                posts.filter(post => post.userId === auth.id).map(post => {
+                history.filter(post => post.userId === auth.id).map(post => {
                     return (
-                        <li key = { `job${ post.id }` }>{ post.id }</li>
+                        <li key = { `job${ post.id }` }><a href={`#post/${post.id}`}>{ post.title }</a></li>
                     )
                 })
             }</ul>
             <h4>Acquired Jobs</h4>
             <ul>{
-                posts.filter(post => post.bidderId === auth.id).map(post => {
+                posts.filter(post => post.acceptedId === auth.id).map(post => {
                     return (
-                        <li key = { `job${ post.id }` }>{ post.id }</li>
+                    <li key = { `job${ post.id }` }><a href={`#post/${post.id}`}>{ post.title }</a></li>
                     )
                 })
             }</ul>
